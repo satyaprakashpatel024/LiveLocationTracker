5f46debe2b4d3e475b1b13745cc208db9449f465
satyaprakashpatel024 <satyaprakashpatel61@gmail.com>
2024-08-16 03:08:34 +0530

    live location tracker project completed

---
+++ index.js
@@ -0,0 +1,32 @@
+const exp = require('constants');
+const express = require('express');
+const app = express();
+const path = require("path");
+
+const http = require('http');
+
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function (socket) {
+    socket.on("send-location", function (data) {
+        io.sockets.emit("receive-location", { id: socket.id, ...data });
+    });
+    socket.on("disconnect",function(){
+        console.log("User disconnected with id: " + socket.id);
+        io.sockets.emit("user-disconnected", socket.id);  // notify all users about the disconnected user  // io.emit() sends a message to all connected clients  // io.to(socket.id).emit() sends a message to a specific client  // io.to(socket.id).broadcast.emit() sends a message to all clients except the sender  // io.to(socket.id).emit() and io.to(socket.id).broadcast.emit() can be used interchangeably.  // io.to(socket.id).emit() sends a message to a specific client, while io.emit() sends a message to all connected clients  // io.to(socket.id).broadcast.emit() sends a message to all clients except the sender  // io.to(socket.id).emit() and io.to(socket.
+    })
+    console.log("connected");
+});
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+server.listen(8080, () => {
+    console.log("Server is running on port 8080");
+})
